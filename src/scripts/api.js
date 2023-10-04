355431eee94fe4aa76cc15e03265be4f881b7bac
Melkor <trjasuchkin@gmail.com>
2023-10-04 21:01:08 +0300

    api class

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,58 +1,70 @@
-import {request, config} from "./utils";
+import {request} from "./utils";
 
-export function getInfo(){
-    return request('/users/me', config)
-}
+class Api {
+    constructor(config) {
+        this.config = config;
+    }
 
-export function getCards(){
-    return request('/cards', config)
-}
-
-export function setProfile(newName, newAbout){
-    let tempConfig = config
-    config.method = 'PATCH';
-    config.body = JSON.stringify({
-        name: newName,
-        about: newAbout
-    })
-    return request('/users/me', tempConfig)
-}
-
-export function setCard(newName, newLink){
-    let tempConfig = config
-    config.method = 'POST';
-    config.body = JSON.stringify({
-        name: newName,
-        link: newLink
-    })
-    return request(`/cards`, tempConfig)
-}
-
-export function deleteCardApi(id){
-    let tempConfig = config
-    config.method = 'DELETE';
-    return request(`/cards/${id}`, tempConfig)
-}
-
-export function upLike(id){
-    let tempConfig = config
-    config.method = 'PUT';
-    return request(`/cards/likes/${id}`, tempConfig)
-}
-
-export function downLike(id){
-    let tempConfig = config
-    config.method = 'DELETE';
-    return request(`/cards/likes/${id}`, tempConfig)
-}
-
-export function editAvatar(link){
-    let tempConfig = config
-    config.method = 'PATCH';
-    config.body = JSON.stringify({
-        avatar:link
-    })
-    return request('/users/me/avatar', tempConfig)
-}
-
-
+    getInfo(){
+        return request('/users/me', config)
+    }
+    
+    getCards(){
+        return request('/cards', config)
+    }
+    
+    setProfile(newName, newAbout){
+        let tempConfig = config
+        config.method = 'PATCH';
+        config.body = JSON.stringify({
+            name: newName,
+            about: newAbout
+        })
+        return request('/users/me', tempConfig)
+    }
+    
+    setCard(newName, newLink){
+        let tempConfig = config
+        config.method = 'POST';
+        config.body = JSON.stringify({
+            name: newName,
+            link: newLink
+        })
+        return request(`/cards`, tempConfig)
+    }
+    
+    deleteCardApi(id){
+        let tempConfig = config
+        config.method = 'DELETE';
+        return request(`/cards/${id}`, tempConfig)
+    }
+    
+    upLike(id){
+        let tempConfig = config
+        config.method = 'PUT';
+        return request(`/cards/likes/${id}`, tempConfig)
+    }
+    
+    downLike(id){
+        let tempConfig = config
+        config.method = 'DELETE';
+        return request(`/cards/likes/${id}`, tempConfig)
+    }
+    
+    editAvatar(link){
+        let tempConfig = config
+        config.method = 'PATCH';
+        config.body = JSON.stringify({
+            avatar:link
+        })
+        return request('/users/me/avatar', tempConfig)
+    }
+  }
+  
+  const api = new Api({
+    baseUrl: 'https://nomoreparties.co/v1/plus-cohort-28',
+    headers: {
+      authorization: '587a6577-aa88-4458-9a8b-aaa868f62a49',
+      'Content-Type': 'application/json',
+    },
+  });
--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,11 +1,3 @@
-export const config = {
-    baseUrl: 'https://nomoreparties.co/v1/plus-cohort-28',
-    headers: {
-      authorization: '587a6577-aa88-4458-9a8b-aaa868f62a49',
-      'Content-Type': 'application/json',
-    },
-};
-
 export function checkResponse(res) {
     if (res.ok) 
         return res.json();
